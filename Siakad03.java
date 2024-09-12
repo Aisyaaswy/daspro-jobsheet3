6e9777b0feebcb033b2b2f9e2a1a893a1686d955
aisyaaswy <aisyaidha@gmail.com>
2024-09-12 10:02:08 +0700

    commit pertanyaan percobaan 1 jobsheet 3

--- Siakad03.java
+++ Siakad03.java
@@ -3,30 +3,38 @@ public class Siakad03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in); 
         String nama, nim;
-        char kelas;
+        String kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS, nilaiAkhir;
         
         System.out.print("Masukkan nama : ");
         nama = sc.nextLine();
         System.out.print("Masukkan NIM : ");
         nim = sc.nextLine();
         System.out.print("Masukkan kelas : ");
-        kelas = sc.nextLine().charAt(0);
+        kelas = sc.nextLine();
         System.out.print("Masukkan nomor absen: ");
         absen = sc.nextByte();
+        
+        double bobotKuis = 0.2;
+        double bobotTugas = 0.15; 
+        double bobotUTS = 0.3;
+        double bobotUAS = 0.35;
 
         System.out.print("Masukkan nilai kuis: ");
         nilaiKuis = sc.nextDouble();
         System.out.print("Masukkan nilai tugas: " );
         nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
-
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) /3;
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextDouble();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextDouble();
+        System.out.println("Masukkan Nilai Akhir:");
+        
+        nilaiAkhir = ((nilaiKuis * bobotKuis) + (nilaiTugas * bobotTugas) + (nilaiUTS * bobotUTS) + (nilaiUAS * bobotUAS));
 
         System.out.print("Nama: " + nama + " NIM: " + nim);
-        System.out.println(" Kelas: " + kelas + " Absen: " + absen);
-        System.out.println("NIlai Akhir: " + nilaiAkhir);
+        System.out.print(" Kelas: " + kelas + " Absen: " + absen);
+        System.out.println(" NIlai Akhir: " + nilaiAkhir);
         }
 }
